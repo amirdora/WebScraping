09ff0b0425490c9d595dce2c371dd56b7d3c199b
Amir Dorazahi <amirdora@mail.uni-paderborn.de>
2021-05-08 17:24:22 +0200

    working version, with click problem

--- first_project/first_project/spiders/my_spider.py
+++ first_project/first_project/spiders/my_spider.py
@@ -12,21 +12,30 @@ def get_all_urls(response):
     return response.xpath("//span[@class='image-frame']/a[1]/@href").extract()
 
 
-def parseProArguments(item, yes_arguments):
+def parseProArguments(driver, item, yes_arguments):
+    count_yes = 1
     for argument in yes_arguments:
-        title = argument.find_element_by_xpath('//h2').text
-        body = argument.find_element_by_xpath('//p').text
+        h2_path = "//div[@class='arguments args-yes']/ul/li[@class='hasData'][%s]/h2" % count_yes
+        p_path = "//div[@class='arguments args-yes']/ul/li[@class='hasData'][%s]/p" % count_yes
 
+        title = driver.find_element(By.XPATH, h2_path).text
+        body = driver.find_element(By.XPATH, p_path).text
+        count_yes = count_yes + 1
         item['pro_arguments'].append({
             'title': title,
             'body': body})
     return item
 
 
-def parseConArguments(item, no_arguments):
+def parseConArguments(driver, item, no_arguments):
+    countNo = 1
     for argument in no_arguments:
-        title = argument.find_element_by_xpath('//h2').text
-        body = argument.find_element_by_xpath('//p').text
+        h2_path = "//div[@class='arguments args-no']/ul/li[@class='hasData'][%s]/h2" % countNo
+        p_path = "//div[@class='arguments args-no']/ul/li[@class='hasData'][%s]/p" % countNo
+
+        title = driver.find_element(By.XPATH, h2_path).text
+        body = driver.find_element(By.XPATH, p_path).text
+        countNo = countNo + 1
 
         item['con_arguments'].append({
             'title': title,
@@ -47,38 +56,35 @@ class CrawlDebatesSpider(scrapy.Spider):
         driver = self.initializingSileniumDriver()
 
         # iterating through 5 urls
-        for url2 in urls[0:5]:
+        for url2 in urls[1:2]:
             # Use headless option to not open a new browser window
             # Getting list of Countries
             concatinated_url = "https://www.debate.org" + url2
             driver.get(concatinated_url)
 
             while True:
-                time.sleep(1)
                 try:
                     self.loadMoreData(driver)
                 except Exception:
                     break
 
             # Extracting country names
-            yes_arguments = driver.find_elements_by_xpath('//*[@id="yes-arguments"]/ul/li')
-            no_arguments = driver.find_elements_by_xpath('//*[@id="no-arguments"]/ul/li')
-
+            yes_arguments = driver.find_elements_by_xpath("//div[@class='arguments args-yes']/ul/li[@class='hasData']")
+            no_arguments = driver.find_elements_by_xpath("//div[@class='arguments args-no']/ul/li[@class='hasData']")
+            topic = driver.find_element_by_class_name('q-title').text
+            category = driver.find_element_by_xpath('//*[@id="breadcrumb"]/a[3]').text
             # main json dictionary
-            item = dict(topic=driver.find_element_by_class_name('q-title').text,
-                        category=driver.find_element_by_xpath('//*[@id="breadcrumb"]/a[2]').text,
-                        pro_arguments=[],
-                        con_arguments=[])
+            item = dict(topic=topic, category=category, pro_arguments=[], con_arguments=[])
 
-            item = parseProArguments(item, yes_arguments)
+            item = parseProArguments(driver, item, yes_arguments)
 
-            item = parseConArguments(item, no_arguments)
+            item = parseConArguments(driver, item, no_arguments)
 
             yield item
         driver.quit()
 
     def loadMoreData(self, driver):
-        element = WebDriverWait(driver, 20).until(
+        element = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="col-wi"]/div/div[5]/a')))
         driver.execute_script("arguments[0].click();", element)
 
@@ -86,5 +92,5 @@ class CrawlDebatesSpider(scrapy.Spider):
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
         desired_capabilities = options.to_capabilities()
-        driver = webdriver.Chrome("/Users/ad/chromedriver")
+        driver = webdriver.Chrome("../chromedriver")
         return driver
